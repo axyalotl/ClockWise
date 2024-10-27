f52984dcc8ee47a9dc23cbc7d9260baee1884520
AlexisP <a.phillip@ufl.edu>
2024-10-26 23:23:06 -0400

    The rest of the APP for frontend

--- frontend/clockwise-frontend/src/App.js
+++ frontend/clockwise-frontend/src/App.js
@@ -23,3 +23,25 @@ function App() {
 }
 
 export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard'; // Protected route
+import SecureRoute from './SecureRoute';  // Import the SecureRoute component
+
+function App() {
+  return (
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          {/* Protect the dashboard route with SecureRoute */}
+          <SecureRoute path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
