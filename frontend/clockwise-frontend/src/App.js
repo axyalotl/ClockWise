10bcf22a133137527bd1194caf2c6cf6e58d4516
JolinaJ <116741984+JolinaJ@users.noreply.github.com>
2024-10-30 21:57:35 -0400

    progress

--- frontend/clockwise-frontend/src/App.js
+++ frontend/clockwise-frontend/src/App.js
@@ -1,11 +1,9 @@
-// Import necessary modules
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import { AuthProvider } from './contexts/AuthContext';
 
-// Import your components
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
@@ -14,28 +12,11 @@ import SecureRoute from './components/SecureRoutes';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-
-            {/* Wrap the Router and provide auth context */}
             <AuthProvider>
                 <Router>
                     <Routes>
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
-                        {/* Protect the dashboard route with SecureRoute */}
                         <Route path="/dashboard" element={<SecureRoute component={Dashboard} />} />
                     </Routes>
                 </Router>
@@ -45,3 +26,4 @@ function App() {
 }
 
 export default App;
+
